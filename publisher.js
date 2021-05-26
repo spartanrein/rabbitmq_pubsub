f88d1ebd295d5e3e0a2731961bd946ee39845397
Rein de Guzman <37677855+spartanrein@users.noreply.github.com>
2021-05-26 16:56:21 +0800

    add interval for publish and replenish data for publisher

--- publisher.js
+++ publisher.js
@@ -1,34 +1,55 @@
 const axios = require('axios');
 const amqp = require('amqplib/callback_api');
 
-var amqp_url = "amqps://tqigunti:Z3lVxmRsW2tedQqhCghElYciBMZTEbQL@mustang.rmq.cloudamqp.com/tqigunti";
+//should be read from an env variable
+const amqp_url = "amqps://tqigunti:Z3lVxmRsW2tedQqhCghElYciBMZTEbQL@mustang.rmq.cloudamqp.com/tqigunti";
 
-function publish(message) {
-    amqp.connect(amqp_url, function (error0, connection) {
-        if (error0) {
-            throw error0
+
+amqp.connect(amqp_url, function (error0, connection) {
+    if (error0) {
+        throw error0
+    }
+    connection.createChannel(async function (error1, channel) {
+        if (error1) {
+            throw error1;
         }
-        connection.createChannel(function (error1, channel) {
-            if (error1) {
-                throw error1;
+
+        const exchange = "message_exchange";
+        channel.assertExchange(exchange, 'direct', {
+            durable: true
+        });
+
+
+        let messages = await getQuotes();
+        messages = formatMessages(messages);
+        console.log(messages);
+
+        setInterval(async () => {
+            let newMessages = await getQuotes();
+            newMessages = formatMessages(newMessages);
+            console.log(newMessages);
+            newMessages.forEach(message => {
+                messages.unshift(message);
+            });
+            console.log(`messages refilled: ${messages.length}`)
+
+        }, 3500);
+
+        //publish 20 messages per second
+        setInterval(() => {
+            try {
+                for (let i = 0; i < 20; i++) {
+                    let data = messages.pop();
+                    channel.publish(exchange, data.severity, Buffer.from(JSON.stringify(data)));
+                    console.log("pushed file");
+                }
+            } catch (err) {
+                console.log(err)
             }
 
-            let exchange = "message_exchange";
-
-            channel.assertExchange(exchange, 'direct', {
-                durable: true
-            });
-
-            channel.publish(exchange, message.severity, Buffer.from(JSON.stringify(message)));
-            console.log(`${JSON.stringify(message)} is published`);
-        });
-        setTimeout(function () {
-            connection.close();
-            process.exit(0);
-
-        }, 500);
+        }, 800);
     });
-}
+});
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -37,20 +58,19 @@ function getRandomInt(min, max) {
 }
 
 async function getQuotes() {
-    const res = await axios.get("https://baconipsum.com/api/?type=all-meat&sentences=20&start-with-lorem=0");
-    return res.data
+    let res = await axios.get("https://baconipsum.com/api/?type=all-meat&sentences=100&start-with-lorem=0");
+    return res.data.toString().split(".");
 }
 
-//
-getQuotes().then(data => {
-    const quotes = data.toString().split(".");
-    for (let i = 0; i < quotes.length - 1; i++) {
+function formatMessages(data) {
+    let messages = [];
+    for (let i = 0; i < data.length - 1; i++) {
         let message = {
-            message: quotes[i],
+            quote: data[i],
             timestamp: Date.now(),
-            severity: (getRandomInt(1, 10) >= 7 ? 'high' : 'low')
+            severity: (getRandomInt(1, 100) >= 7 ? 'high' : 'low')
         };
-        publish(message)
+        messages.push(message);
     }
-
-});
+    return messages
+}
